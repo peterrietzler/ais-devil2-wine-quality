634e3f039c03d71d4b016eb1847caa02cca368a5
mrcohrtl <s2510929014@fhooe.at>
2026-05-29 15:03:34 +0200

    data successfully added

--- data_information.py
+++ data_information.py
@@ -5,6 +5,6 @@ import os
 data = pd.read_parquet('data/winequality.parquet')
 print(data.dtypes)
 
-data_size  = os.path.getsize('example-data/2025-01-01.taxi-rides.parquet')
+data_size  = os.path.getsize('data/winequality.parquet')
 
 print(f'Data Size: {data_size  / 1024 / 1024:.1f} MB')
